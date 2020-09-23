aaef8bd659f9fca03a6562f25d3649f8783a51ab
Rishabh Ballal <rishabhballal@gmail.com>
2020-09-23 09:54:21 +0530

    initial commit

---
+++ eff_loop.py
@@ -0,0 +1,20 @@
+def iter_range(a):
+    v = [0, len(a)-1]
+    for i in range(len(a)):
+        if sum(a[i]):
+            v[0] = i
+            break
+    for i in range(len(a)-1, 0, -1):
+        if sum(a[i]):
+            v[1] = i+1
+            break
+    h = [0, len(a)-1]
+    for i in range(len(a)):
+        if sum(a[:, i]):
+            h[0] = i
+            break
+    for i in range(len(a)-1, 0, -1):
+        if sum(a[:, i]):
+            h[1] = i+1
+            break
+    return v, h
