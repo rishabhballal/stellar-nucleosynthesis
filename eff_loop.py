5df3f8bff7d62ada14ee80d14c32aeed27e72c37
Rishabh Ballal <rishabhballal@gmail.com>
2020-09-24 22:01:43 +0530

    reformatted code

--- eff_loop.py
+++ eff_loop.py
@@ -1,13 +1,4 @@
 def iter_range(a):
-    v = [0, len(a)-1]
-    for i in range(len(a)):
-        if sum(a[i]):
-            v[0] = i
-            break
-    for i in range(len(a)-1, 0, -1):
-        if sum(a[i]):
-            v[1] = i+1
-            break
     h = [0, len(a)-1]
     for i in range(len(a)):
         if sum(a[:, i]):
@@ -17,4 +8,13 @@ def iter_range(a):
         if sum(a[:, i]):
             h[1] = i+1
             break
-    return v, h
+    v = [0, len(a)-1]
+    for i in range(len(a)):
+        if sum(a[i]):
+            v[0] = i
+            break
+    for i in range(len(a)-1, 0, -1):
+        if sum(a[i]):
+            v[1] = i+1
+            break
+    return h, v
