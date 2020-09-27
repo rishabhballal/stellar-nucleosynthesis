8bc383b4cc5c3545184d787ef31e0fcce64c4a25
Rishabh Ballal <rishabhballal@gmail.com>
2020-09-27 19:12:05 +0530

    corrected algorithm

--- centre_of_mass.py
+++ centre_of_mass.py
@@ -1,47 +1,29 @@
 import numpy as np
-from eff_loop import iter_range
+from positions import particle_positions
 
-def centre_of_mass(a):
-    m = sum(sum(a))
-    cm = np.array([0, 0])
-
-    h, v = iter_range(a)
-
+def centre_of_mass(a, pos):
     cm = np.array([0, 0], dtype=float)
 
-    cm_h = np.zeros(len(a))
-    for i in range(h[0], h[1]):
-        for j in range(v[0], v[1]):
-            cm_h[i] += j*a[i, j]
-        cm_h[i] /= sum(a[:, i])
-
-    cm_v = np.zeros(len(a))
-    for i in range(v[0], v[1]):
-        for j in range(h[0], h[1]):
-            cm_v[i] += j*a[i, j]
-        cm_v[i] /= sum(a[i])
-
-    for i in range(len(a)):
-        cm[0] += i*cm_h[i]
-        cm[1] += i*cm_v[i]
-    cm[0] /= sum(cm_h)
-    cm[1] /= sum(cm_v)
+    for i in pos:
+        cm += i*a[i[0], i[1]]
+    cm /= sum(sum(a))
 
     return np.round(cm)
 
 # TEST SECTION
-#
-# dim = 20
-# bound = 2
-#
-# z = np.zeros((dim, dim))
-# n = np.zeros((dim, dim))
-# for i in range(bound, dim-bound):
-#     for j in range(bound, dim-bound):
-#         z[i, j] = np.round(np.random.rand(1))
-#         if z[i, j]:
-#             n[i, j] = np.round(np.random.rand(1))
-# a = z + n
-# print(a)
-#
-# print(centre_of_mass(a))
+
+dim = 20
+bound = 2
+
+z = np.zeros((dim, dim))
+n = np.zeros((dim, dim))
+for i in range(bound, dim-bound):
+    for j in range(bound, dim-bound):
+        z[i, j] = np.random.randint(2)
+        if z[i, j]:
+            n[i, j] = np.random.randint(2)
+a = z + n
+print(a)
+
+pos = particle_positions(a)
+print(centre_of_mass(a, pos))
