1fe99fea8d49a116371e2b6ef3de0ab81a0d390e
Rishabh Ballal <rishabhballal@gmail.com>
2020-10-01 21:54:57 +0530

    completed force function

--- gravity.py
+++ gravity.py
@@ -1,5 +1,22 @@
-def gravity(a, pos):
+import numpy as np
+
+mag = lambda vec: np.sqrt(np.inner(vec, vec))
+
+def force(a, pos):
     grav = np.array([0, 0], dtype=float)
+
     for i in pos:
+        f = np.array([0, 0], dtype=float)
         for j in pos:
-            pass
+            rvec = j - i
+            if rvec.any():
+                f += a[i[0], i[1]]*a[j[0], j[1]]*rvec/mag(rvec)
+        grav = np.vstack((grav, f))
+    grav = grav[1:]
+
+    for i in range(len(grav)):
+        norm = abs(grav[i, 0]) + abs(grav[i, 1])
+        grav[i, 0] /= norm
+        grav[i, 1] /= norm
+
+    return np.round(grav, 2)
