c477f74a481a1473013eb33f8e841f4858f28819
Rishabh Ballal <rishabhballal@gmail.com>
2020-10-01 21:59:18 +0530

    added code to test modules

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from mpl_toolkits.mplot3d import axes3d
 
 import matrix
 import density
+import gravity
 
+z, n, a = matrix.generator()
+print('\nATOMIC MASS NUMBER MATRIX')
+print(a)
 
-mag = lambda vec: np.sqrt(np.inner(vec, vec))
+pos = matrix.positions(a)
+cm = matrix.centre_of_mass(a, pos)
+print('\nCENTRE OF MASS: ', cm)
+
+dens = density.matrix(a)
+print('\nDENSITY MATRIX')
+print(dens)
+
+density.plot(dens)
+density.profile(dens, cm)
+
+grav = gravity.force(a, pos)
+
+print()
