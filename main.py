f1f3121d87df6c98dbb527a2938a14cb6a5a3649
Rishabh Ballal <rishabhballal@gmail.com>
2020-10-08 21:37:56 +0530

    imported core module

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from mpl_toolkits.mplot3d import axes3d
 import matrix
 import density
 import gravity
+import core
 
 z, n, a = matrix.generator()
 print('\nINITIAL ATOMIC MASS NUMBER MATRIX')
@@ -24,6 +25,10 @@ for t in range(1, time+1):
     pos = matrix.positions(a)
     cm = matrix.centre_of_mass(a, pos)
 
+    temp = core.temp(a, cm)
+    if temp > 7:
+        pass
+
     grav = gravity.force(a, pos)
     for i in range(len(grav)):
         r = np.random.rand(1)
@@ -31,14 +36,11 @@ for t in range(1, time+1):
 
         if r <= abs(grav[i, 0]):
             dir = int(grav[i, 0]/abs(grav[i, 0]))
-
             if a[j+dir, k] < a[j, k]:
                 z[j, k], z[j+dir, k] = z[j+dir, k], z[j, k]
                 n[j, k], n[j+dir, k] = n[j+dir, k], n[j, k]
-
         else:
             dir = int(grav[i, 1]/abs(grav[i, 1]))
-
             if a[j, k+dir] < a[j, k]:
                 z[j, k], z[j, k+dir] = z[j, k+dir], z[j, k]
                 n[j, k], n[j, k+dir] = n[j, k+dir], n[j, k]
@@ -53,5 +55,3 @@ cm = matrix.centre_of_mass(a, pos)
 dens = density.matrix(a)
 density.plot(dens)
 density.profile(dens, cm)
-
-print()
