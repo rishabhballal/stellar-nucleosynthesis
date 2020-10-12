8424f2962b8f7b6ecdf685f54404dd319e86f02c
Rishabh Ballal <rishabhballal@gmail.com>
2020-10-12 21:08:11 +0530

    started nuclear fusion code

--- main.py
+++ main.py
@@ -5,28 +5,59 @@ from mpl_toolkits.mplot3d import axes3d
 import matrix
 import density
 import gravity
+import nuclear
 
-z, n, a = matrix.generator()
+dim = 20
+
+z, n, a = matrix.generate(dim)
 print('\nINITIAL ATOMIC MASS NUMBER MATRIX')
 print(a)
 
-pos = matrix.positions(a)
-cm = matrix.centre_of_mass(a, pos)
-print('\nCENTRE OF MASS: ', cm)
+# pos = matrix.positions(a)
+# cm = matrix.centre_of_mass(a, pos)
+# print('\nCENTRE OF MASS: ', cm)
+#
+# dens = density.matrix(a)
+# density.plot(dens)
 
-dens = density.matrix(a)
-density.plot(dens)
+en = 0*a
 
 time = 20
 print('\nITERATIONS')
-for t in range(1, time+1):
-    print(t)
+for t in range(time):
+    print(t+1)
+
     pos = matrix.positions(a)
     cm = matrix.centre_of_mass(a, pos)
+    core, c_temp = matrix.core_temp(a, cm)
 
-    temp = matrix.core_temp(a, cm)
-    if temp > 7:
-        pass
+    if c_temp > 7:
+        c_pos = matrix.positions(core)
+        print(c_pos)
+        # for i in c_pos:
+        #     j = i.copy()
+        #
+        #     r = np.random.randint(2, size=2)
+        #     # r[0] determines the axis
+        #     # horizontal axis: r[0] = 0, vertical axis: r[0] = 1
+        #     # r[1] determines the direction along the axis
+        #     dir = (-1)**r[1]
+        #     if r[0]:
+        #         j[0] += dir
+        #     else:
+        #         j[1] += dir
+        #
+        #     p1 = [z[i[0], i[1]], n[i[0], i[1]]]
+        #     p2 = [z[j[0], j[1]], n[j[0], j[1]]]
+        #     print(p1, p2)
+        #     e = en[i[0], i[1]]
+        #     rn = nuclear.reaction(p1, p2, e)
+        #     z[i[0], i[1]] = rn[0]
+        #     n[i[0], i[1]] = rn[1]
+        #     z[j[0], j[1]] = rn[2]
+        #     n[j[0], j[1]] = rn[3]
+        #     a = z + n
+        #     en[i[0], i[1]] = rn[4]
 
     grav = gravity.force(a, pos)
     for i in range(len(grav)):
@@ -48,9 +79,9 @@ for t in range(1, time+1):
 print('\nFINAL ATOMIC MASS NUMBER MATRIX')
 print(a)
 
-pos = matrix.positions(a)
-cm = matrix.centre_of_mass(a, pos)
-
-dens = density.matrix(a)
-density.plot(dens)
-density.profile(dens, cm)
+# pos = matrix.positions(a)
+# cm = matrix.centre_of_mass(a, pos)
+#
+# dens = density.matrix(a)
+# density.plot(dens)
+# density.profile(dens, cm)
