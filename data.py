c72f1fbf1dc492f1938d5c54c78575c7a0b0b064
Rishabh Ballal <rishabhballal@gmail.com>
2020-10-16 17:54:40 +0530

    added plot function and corrected composition function

--- data.py
+++ data.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def composition(z, pos):
     # [atomic number, percentage by mass]
-    comp = np.array([0, 0], dtype=float)
-    for i in range(1, z.max()):
-        comp = np.vstack((comp, [i, 0]))
+    comp = np.array([[0, 0], [1, 0]], dtype=float)
+    for i, j in pos:
+        if z[i, j] not in comp[:, 0]:
+            comp = np.vstack((comp, [z[i, j], 0]))
+    comp = comp[1:]
 
     # counting their occurence
     for i in range(len(comp)):
@@ -14,7 +17,15 @@ def composition(z, pos):
                 comp[i, 1] += 1
         comp[i, 1] *= comp[i, 0] * 100/z.sum()
 
-    return np.round(comp[1:], 2)
+    # sorting according to atomic number
+    for i in range(len(comp)):
+        for j in range(i+1, len(comp)):
+            if comp[i, 0] > comp[j, 0]:
+                comp[i, 0], comp[j, 0] = comp[j, 0], comp[i, 0]
+                comp[i, 1], comp[j, 1] = comp[j, 1], comp[i, 1]
+
+    return np.round(comp, 2)
+
 
 def log(comp, type, iter):
     with open('log.txt', type) as file:
@@ -22,3 +33,15 @@ def log(comp, type, iter):
         for i in comp:
             file.write('%d: %.2f\n' %(i[0], i[1]))
         file.write('\n')
+
+
+def plot(elm, iter):
+    labels = ['Hydrogen', 'Helium', 'Carbon', 'Nitrogen', 'Oxygen']
+    fig, ax = plt.subplots()
+    for i in range(len(elm[0])):
+        ax.plot(range(iter), elm[:, i], label=labels[i])
+    ax.set_xlabel('Iterations')
+    ax.set_ylabel('Percentage by mass')
+    ax.grid()
+    ax.legend()
+    plt.savefig('images/data.jpg')
