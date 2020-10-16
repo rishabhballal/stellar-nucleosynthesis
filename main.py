4643453c0eaf2e0dd29279221932b96d0626288e
Rishabh Ballal <rishabhballal@gmail.com>
2020-10-16 17:52:56 +0530

    completed data handling

--- main.py
+++ main.py
@@ -5,75 +5,51 @@ import gravity
 import nuclear
 import data
 
+# ignores runtime warning for ZeroDivisionError, as error is already handled
 np.seterr(divide='ignore')
 
-# minimum = 10
+# matrix side length; minimum = 10
 dim = 20
-
+# proton number, neutron number, atomic number matrices
 z, n, a = matrix.generate(dim)
-en = 0*a
-elm = np.array([['Hydrogen', 'Helium', 'Carbon', 'Nitrogen', 'Oxygen'], [1, 2, 6, 7, 8]])
+# energy matrix
+en = np.zeros((len(a), len(a)))
+# data handling arrays
+elm = np.array([1, 2, 6, 7, 8])
+stack = np.zeros(len(elm))
+# total atomic mass
+print('\nMass: %d' %a.sum())
 
-m = a.sum()
-print('\nMass: ', m)
-
+# initial system
 pos = matrix.positions(a)
 cm = matrix.centre_of_mass(a, pos)
-
 dens = density.matrix(a)
 density.plot(dens, 'initial')
 
+# data handling
 comp = data.composition(z, pos)
+for i in range(len(stack)):
+    for j in range(len(comp)):
+        if elm[i] == comp[j, 0]:
+            stack[i] = comp[j, 1]
+            break
+    else:
+        stack[i] = 0
+elm = np.vstack((elm, stack))
 data.log(comp, 'w', 0)
 
+# control variables
+flag1 = True
 flag2 = False
-time = 20
-print('\nIterations:')
-for t in range(time):
-    print(t+1)
+iter = 0
+print('\nIterations:\n')
+while flag1:
+    iter += 1
+    print(iter, end='. ')
 
+    # gravitational attraction
     pos = matrix.positions(a)
-    cm = matrix.centre_of_mass(a, pos)
-    c_pos, c_temp = matrix.core(a, pos, cm)
-
-    if not flag2:
-        if c_temp > 7:
-            flag2 = True
-    else:
-        for i in c_pos:
-            j = i.copy()
-
-            r = np.random.randint(2, size=2)
-            # r[0] determines the axis (horizontal: 0, vertical: 1)
-            # r[1] determines the direction along the axis
-            dir = (-1)**r[1]
-            if r[0]:
-                j[0] += dir
-            else:
-                j[1] += dir
-
-            p1 = [z[i[0], i[1]], n[i[0], i[1]]]
-            p2 = [z[j[0], j[1]], n[j[0], j[1]]]
-            try:
-                f = c_temp/(p1[0]*p2[0])
-                if f > 1:
-                    f = 1
-            except ZeroDivisionError:
-                f = 1
-            e = en[i[0], i[1]]
-
-            nr = nuclear.reaction(p1, p2, f, e)
-            z[i[0], i[1]] = nr[0]
-            n[i[0], i[1]] = nr[1]
-            z[j[0], j[1]] = nr[2]
-            n[j[0], j[1]] = nr[3]
-            a = z + n
-            en[i[0], i[1]] = nr[4]
-
-    pos = matrix.positions(a)
-    data.log(data.composition(z, pos), 'a', t+1)
     grav = gravity.force(a, pos)
-
     for i in range(len(grav)):
         r = np.random.rand(1)
         j, k = pos[i]
@@ -90,9 +66,62 @@ for t in range(time):
                 n[j, k], n[j, k+dir] = n[j, k+dir], n[j, k]
         a = z + n
 
+    # nuclear fusion
+    pos = matrix.positions(a)
+    cm = matrix.centre_of_mass(a, pos)
+    c_pos, c_temp = matrix.core(a, pos, cm)
+    if not flag2:
+        if c_temp > 7:
+            flag2 = True
+    else:
+        for i in c_pos:
+            j = i.copy()
+            r = np.random.randint(2, size=2)
+            # r[0] determines the axis (horizontal: 0, vertical: 1)
+            # r[1] determines the direction along the axis
+            dir = (-1)**r[1]
+            if r[0]:
+                j[0] += dir
+            else:
+                j[1] += dir
+            p1 = [z[i[0], i[1]], n[i[0], i[1]]]
+            p2 = [z[j[0], j[1]], n[j[0], j[1]]]
+            try:
+                f = c_temp/(p1[0]*p2[0])
+                if f > 1:
+                    f = 1
+            except ZeroDivisionError:
+                f = 1
+            e = en[i[0], i[1]]
+            nr = nuclear.reaction(p1, p2, f, e)
+            z[i[0], i[1]] = nr[0]
+            n[i[0], i[1]] = nr[1]
+            z[j[0], j[1]] = nr[2]
+            n[j[0], j[1]] = nr[3]
+            a = z + n
+            en[i[0], i[1]] = nr[4]
+
+    # data handling
+    comp = data.composition(z, pos)
+    for i in range(len(stack)):
+        for j in range(len(comp)):
+            if elm[0, i] == comp[j, 0]:
+                stack[i] = comp[j, 1]
+                break
+        else:
+            stack[i] = 0
+    elm = np.vstack((elm, stack))
+    data.log(comp, 'a', iter)
+
+    # iterates till H + He concentration drops below 5%
+    print('Hydrogen + Helium: %.2f\n' %(stack[0] + stack[1]))
+    if stack[0] + stack[1] < 5:
+        flag1 = False
+
+# final system
 pos = matrix.positions(a)
 cm = matrix.centre_of_mass(a, pos)
-
 dens = density.matrix(a)
 density.plot(dens, 'final')
 density.profile(dens, cm)
+data.plot(elm[1:], iter+1)
